fea617cef8205dada6ebc8d6d3027bf866985217
mesuhailpm <tosuhailpm@gmail.com>
2023-07-03 23:47:28 +0530

    commit

--- app/layout.js
+++ app/layout.js
@@ -1,22 +1,27 @@
 import '@styles/global.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
   title: 'Promtpedia',
   description: 'Discover and share AI promts',
 }
- 
+
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <body>
-      <div className='main'>
-        <div className="" />
-      </div>
-      <main className="app">
+        <Provider>
+          <div className='main'>
+            <div className="gradient" />
+          </div>
+          <main className="app">
+          <Nav />
 
-        {children}
-      </main>
-      
+            {children}
+          </main>
+        </Provider>
+
       </body>
     </html>
   )
