41ee25abeb4cc6d74b5ef616b86c357395616bf9
suhailpm <tosuhailpm@gmail.com>
2023-07-10 07:33:01 +0530

    bug fix

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -31,7 +31,7 @@ const Nav = () => {
       <div className="sm:flex hidden">
         {session?.user
         ? (<div className='flex gap-3 md-gap-5'>
-              <Link href='create-prompt' className='black_btn'>
+              <Link href='/create-prompt' className='black_btn'>
                 Create a prompt
               </Link>
               <button type='button' onClick={signOut}>
