bed61d82d93b72c4e7516ea042a467155b51e906
mesuhailpm <tosuhailpm@gmail.com>
2023-07-10 22:36:05 +0530

    commit

--- app/api/users/[id]/prompts/route.js
+++ app/api/users/[id]/prompts/route.js
@@ -3,13 +3,9 @@ import { connectToDb } from "@utils/database"
 
 export const GET = async (req,{params} ) => {
     try {
-        console.log('hello')
         await connectToDb()
         const {id}  = params
-        console.log(id)
         const posts = await Prompt.find({creator: id}).populate('creator')
-        console.log(posts, 'is posts')
-
         return new Response(JSON.stringify(posts), {status: 200})
     } catch (error) {
         return new Response(error, {status:501})
