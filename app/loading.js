f6f2beb23f65a033acdf8fc8901f4488d23b2724
suhailpm <tosuhailpm@gmail.com>
2023-07-11 13:24:48 +0530

    loading.jsx for home page

---
+++ app/loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Loading from '@components/Loading'
+
+const loading = () => {
+  return (
+    <Loading type='InfinitySpin'/>
+  )
+}
+
+export default loading
---
+++ components/Loading.jsx
@@ -0,0 +1,37 @@
+'use client'
+import React from 'react'
+import {MagnifyingGlass, InfinitySpin, Oval, Comment} from 'react-loader-spinner'
+
+const Loading = ({type}) => {
+
+    console.log('Loading type: ' + type)
+    const defaultLoading = () =>{
+        return (<>Wait, we are Loading...</>)
+    }
+
+    let Component;
+
+    switch (type) {
+        case 'MagnifyingGlass':
+            Component = MagnifyingGlass
+            break;
+        case 'InfinitySpin':
+            Component = InfinitySpin;
+            break;
+        case 'Oval':
+            Component = Oval;
+            break;
+        case 'Comment':
+            Component = Comment;
+            break;
+    
+        default:
+            Component = defaultLoading;
+            break;
+
+    }
+    
+    return Component ? <Component /> : null
+}
+
+export default Loading
