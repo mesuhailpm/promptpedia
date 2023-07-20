4475cd3053998186225d32c00ff93b5e57215e4f
suhailpm <tosuhailpm@gmail.com>
2023-07-20 20:52:43 +0530

    formatting

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,117 +1,106 @@
-'use client';
+"use client";
 
 import { useEffect, useState } from "react";
 import PromptCard from "./PromptCard";
-import Loading from '@components/Loading';
+import Loading from "@components/Loading";
 
 const Feed = () => {
+  const [prompts, setPrompts] = useState([]); // all prompts
+  const [filteredPrompts, setFilteredPrompts] = useState([]);
+  const [copied, setcopied] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const handleCopy = async (text) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        setcopied(text);
+        setTimeout(() => {
+          setcopied("");
+        }, 5000);
+      })
+      .catch((error) => console.log(error));
+  };
 
-  const [prompts, setPrompts] = useState([]) // all prompts
-  const [filteredPrompts, setFilteredPrompts] = useState ([])
-  const [copied, setcopied] = useState('')
-  const [searchText, setSearchText] = useState('')
-  const [searchTimeout, setSearchTimeout] = useState(null)
-  const handleCopy = async (text) =>{
-    navigator.clipboard.writeText(text)
-    .then(()=> {
-      setcopied(text)
-      setTimeout(()=>{
-        setcopied('')
-      },5000)})
-    .catch((error) => console.log(error))
-
-    
-  }
-  
   const filterPrompts = (keyword) => {
-    
     setFilteredPrompts(
-      prompts.filter((prompt)=> 
-      prompt.creator.username === keyword ||
-      prompt.tag === keyword ||
-      prompt.prompt == keyword
-      ))
-    }
-    
-  const handleTagSearch = (tag) => {
-    console.log('serching for', tag)
-    setSearchText(tag)
-    filterPrompts(tag)
+      prompts.filter(
+        (prompt) =>
+          prompt.creator.username === keyword ||
+          prompt.tag === keyword ||
+          prompt.prompt == keyword
+      )
+    );
+  };
 
-  }
-  const handleSearch = (e) =>{
-    
+  const handleTagSearch = (tag) => {
+    console.log("serching for", tag);
+    setSearchText(tag);
+    filterPrompts(tag);
+  };
+  const handleSearch = (e) => {
     clearTimeout(searchTimeout);
     setSearchText(e.target.value);
-    
+
     setSearchTimeout(
-      setTimeout(()=>{
+      setTimeout(() => {
         filterPrompts(e.target.value);
-    },1000))
-  }
-  const PromptCardList = ({prompts}) => {
-    return <div className="mt-16 prompt_layout">
-
-            {prompts?.map((prompt,index)=>(
-              <PromptCard
-                key={index}
-                post={prompt}
-                copied={copied}
-                handleCopy={handleCopy}
-                handleTagSearch={handleTagSearch}
-                />
-            ))}
-        </div>
-  }
-
-
-  
+      }, 1000)
+    );
+  };
+  const PromptCardList = ({ prompts }) => {
+    return (
+      <div className="mt-16 prompt_layout">
+        {prompts?.map((prompt, index) => (
+          <PromptCard
+            key={index}
+            post={prompt}
+            copied={copied}
+            handleCopy={handleCopy}
+            handleTagSearch={handleTagSearch}
+          />
+        ))}
+      </div>
+    );
+  };
 
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch('api/prompt')
+        const response = await fetch("api/prompt");
         const data = await response.json();
-        setPrompts(data)
-        
+        setPrompts(data);
       } catch (error) {
-        console.log('unable to fetch posts error: ', error)
+        console.log("unable to fetch posts error: ", error);
       }
+    };
 
-    }
+    fetchPosts();
+  }, []);
 
-    fetchPosts()
+  if (!prompts.length) {
+    return <Loading type="Comment" />;
+  }
 
-  },[])
-
-    if(!prompts.length) { return <Loading type = 'Comment' />}
-
-    return (
-      <section className="feed">
-        <form className="relative w-full flex-center">
-          <input type="text" 
-            className="search_input peer"
-            placeholder="Search for tags, prompts, and users..."
-            value={searchText}
-            onChange={handleSearch}
-            required
-          />
-        </form>
-        {
-          searchText 
-          ?(
-            <PromptCardList
-              prompts={filteredPrompts} 
-            />
-          )
-          :
-            (<PromptCardList 
-              prompts = {prompts}
-            />)
-        }
-        
-      </section>
-    );
-  };
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          className="search_input peer"
+          placeholder="Search for tags, prompts, and users..."
+          value={searchText}
+          onChange={handleSearch}
+          required
+        />
+      </form>
+      {searchText ? (
+        <PromptCardList prompts={filteredPrompts} />
+      ) : (
+        <PromptCardList prompts={prompts} />
+      )}
+    </section>
+  );
+};
 
 export default Feed;
